b5efaf943eca13147c94c1bb9819ce9271df8937
quickhand92 <simon.tran7@gmail.com>
2023-05-02 18:51:44 -0400

    Exercise 3.6 completed

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,16 @@
 // @ts-nocheck
 const express = require('express')
-const app = express.app()
+const app = express()
 
-const persons = [
+app.use(express.json())
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -25,3 +33,61 @@ const persons = [
     }
 ]
 
+app.get('/', (request, response) => {
+    response.send('Main Page')
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length} people<div><div>${new Date}<div>`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const requestedID = Number(request.params.id)
+    const requestedPerson = persons.find(person => person.id == requestedID)
+    if (!requestedPerson) {
+        response.status(404).send('Not found')
+    } else {
+        response.send(requestedPerson)
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const requestedID = Number(request.params.id)
+    const requestedPerson = persons.find(person => person.id == requestedID)
+    if (!requestedPerson) {
+        response.status(404).send('Not found')
+    } else {
+        persons = persons.filter(person => person.id != requestedID)
+        response.status(200).send('Successfully deleted')
+    }
+})
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+    const newID = Math.floor((Math.random() * 100000000))
+
+    if (!body || !body.number || !body.name) {
+        return response.status(400).json({
+            error: 'Number or name is missing from request.'
+        })
+    }
+
+    if (persons.find(person => person.name == body.name)) {
+        return response.status(409).json({
+            error: 'Name already exists in phonebook'
+        })
+    }
+    else {
+        const personObject = {
+            id: newID,
+            name: body.name,
+            number: body.number
+        }
+        persons = persons.concat(personObject)
+        return response.status(200).json(request.body)
+    }
+})
